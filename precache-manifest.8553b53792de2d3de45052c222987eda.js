e04aae9bba4976d3362c4dac18c917722605ff2d
NaveenB <naveen.bandhu@gmail.com>
2020-06-24 16:18:30 +0200

    fixed home path

---
+++ precache-manifest.8553b53792de2d3de45052c222987eda.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "d08b0e53ff4605fbcc2b",
+    "url": "./static/css/main.5584d9c7.chunk.css"
+  },
+  {
+    "revision": "d08b0e53ff4605fbcc2b",
+    "url": "./static/js/main.acde005e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "eda1783d779400137702",
+    "url": "./static/css/2.60ae034f.chunk.css"
+  },
+  {
+    "revision": "eda1783d779400137702",
+    "url": "./static/js/2.afd77beb.chunk.js"
+  },
+  {
+    "revision": "bd13ffcd1cc2e55fc72742e0eefd4d6b",
+    "url": "./static/media/microsoft.bd13ffcd.png"
+  },
+  {
+    "revision": "d947289c97aeb4d66c66a27e286246a1",
+    "url": "./static/media/mathematics.d947289c.gif"
+  },
+  {
+    "revision": "de6e8c814dadf657f66fadacaea1a2e0",
+    "url": "./static/media/english.de6e8c81.png"
+  },
+  {
+    "revision": "6bef65352bf474ce5121e1484128e425",
+    "url": "./static/media/german.6bef6535.png"
+  },
+  {
+    "revision": "e6c186d66f5b99fca7f93cf6288c7584",
+    "url": "./static/media/hindi.e6c186d6.png"
+  },
+  {
+    "revision": "9eac2d5f06aefc3ca484681b2c8c60cd",
+    "url": "./static/media/history.9eac2d5f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d0567605624e21241ab510cd07af4b63",
+    "url": "./static/media/google.d0567605.png"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "./static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "./static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "./static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "./static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "./static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "./static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "./static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "./static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "./static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "./static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "./static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "./static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "./static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "./static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "./static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "bf5d472dcc223e1b110f0103f5fa8c3e",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "mygurukool.github.io/eduApp/precache-manifest.2a36badc4c86d32765fc1e74e667282c.js"
+  "./precache-manifest.8553b53792de2d3de45052c222987eda.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("mygurukool.github.io/eduApp/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
